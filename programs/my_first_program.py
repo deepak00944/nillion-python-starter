5bc1c9dfbecf19e07ece8d2a5d337dd907c9e3db
deepak00944 <deepak.s.ashta@gmail.com>
2024-06-26 10:56:13 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,23 @@
-from nada_dsl import *
+from nada_dsl import Party, SecretInteger, Input, Output
 
+class ComputationProgram:
+    def __init__(self, party_name):
+        self.party = Party(name=party_name)
+        self.input1 = self.create_input("my_int1")
+        self.input2 = self.create_input("my_int2")
+
+    def create_input(self, name):
+        return SecretInteger(Input(name=name, party=self.party))
+
+    def compute(self):
+        # Implement your computation logic here using self.input1 and self.input2
+        result = self.input1  # Example computation, replace with actual logic
+        return result
+
+    def get_output(self):
+        result = self.compute()
+        return [Output(result, "my_output", self.party)]
 
 def nada_main():
-    party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
-
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
-
-    return [Output(my_int1, "my_output", party1)]
+    program = ComputationProgram("Party1")
+    return program.get_output()
